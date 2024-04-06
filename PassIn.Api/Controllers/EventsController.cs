453d3ca8c28a4526f754c2a71ff8df4436250178
Denner <dennermpb@hotmail.com>
2024-04-06 13:24:16 -0300

    First commit

---
+++ PassIn.Api/Controllers/EventsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PassIn.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EventsController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register()
+    {
+        return Created();
+    }
+}
